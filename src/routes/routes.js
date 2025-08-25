d4aff0aa4028089cdba237b80b55659b3d5cac80
ArthurDombroski <tutudombroski@gmail.com>
2025-08-25 17:30:24 -0300

    Alterei o arquivo index para routes e importei todas as rotas nele

---
+++ src/routes/routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import animalRoutes from "./animalRoutes";
+import adminRoutes from "./adminRoutes";
+import doacaoRoutes from "./doacaoRoutes";
+import loginRoutes from "./loginRoutes";
+import pedidoAdocaoRoutes from "./pedidoAdocaoRoutes";
+import usuarioRoutes from "./usuarioRoutes";
