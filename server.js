12a01a7330fd827bfe33b60199503de5eb86e474
ArthurDombroski <tutudombroski@gmail.com>
2025-08-25 17:47:56 -0300

    importei routes no server.js e continuei o código

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import routes from "./src/routes/routes";
 
 const app = express();
 const port = 3000;
+const routes = require("./src/routes/routes");
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`servidor rodando em http://localhost:${port}`);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,3 +5,16 @@ import doacaoRoutes from "./doacaoRoutes";
 import loginRoutes from "./loginRoutes";
 import pedidoAdocaoRoutes from "./pedidoAdocaoRoutes";
 import usuarioRoutes from "./usuarioRoutes";
+
+
+const router = express.Router();
+
+// Registrando cada rota com um caminho base
+router.use("/animals", animalRoutes);
+router.use("/admin", adminRoutes);
+router.use("/doacoes", doacaoRoutes);
+router.use("/login", loginRoutes);
+router.use("/pedidos-adocao", pedidoAdocaoRoutes);
+router.use("/usuarios", usuarioRoutes);
+
+export default router;
