8ca86eef0f54afd64daf2bf1c8dbd6518574da07
ArthurDombroski <tutudombroski@gmail.com>
2025-09-02 09:34:07 -0300

    feat(login): adicionar rota POST em loginRoutes

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,1 +1,6 @@
 import express from "express"
+import usuarioController from "../controllers/usuarioController.js";
+
+const router = express.Router();
+
+router.post("/cadastrar", usuarioController.cadastrar); //não sei se ta certo, mas tem teoria é só isso, pede só o método post
