bce44685ab5bc0c594eb38559e357bf9ef0bfa30
ArthurDombroski <tutudombroski@gmail.com>
2025-09-06 19:32:09 -0300

    feat: router e default criado nas rotas que faltavam

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -4,3 +4,5 @@ import usuarioController from "../controllers/usuarioControllers.js";
 const router = express.Router();
 
 router.post("/cadastrar", usuarioController.cadastrar); //não sei se ta certo, mas tem teoria é só isso, pede só o método post
+
+export default router;
