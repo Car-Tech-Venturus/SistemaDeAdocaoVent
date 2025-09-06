47dbf577fae7fe5cd4ca302f62057ade568c5ee3
ArthurDombroski <tutudombroski@gmail.com>
2025-09-06 14:24:35 -0300

    refactor(animal): atualizada para ficar condizente com o Controllers e com os requisitos do projeto

--- src/routes/animalRoutes.js
+++ src/routes/animalRoutes.js
@@ -3,22 +3,27 @@ import animalController from "../controllers/animalController.js";
 
 const router = express.Router();
 
-// Lista todos os animais
-router.get("/", animalController.listar);
+//Separei primeiras as rotas de admin e dps as rotas públicas, para ficar melhor identado
+//Também pq se eu não me engano, o express lê as rotas de cima pra baixo
 
-// Lista animal por ID
-router.get("/:id", animalController.buscarPorId);
+// Listar todos os animais (Admin)
+router.get("/admin/animais", animalController.listarAdmin);
 
-// Lista todos os animais (rota admin)
-router.get("/admin/animais", animalController.listarAdmin);       
-
-// Cadastra animal
-router.post("/", animalController.cadastrar);
-
-// Atualizar animal
+// Atualizar animal (Admin)
 router.patch("/admin/animais/:id", animalController.atualizar);
 
-// Deletar animal
+// Deletar animal (Admin)
 router.delete("/admin/animais/:id", animalController.deletar);
 
+
+// Buscar animal por ID
+router.get("/:id", animalController.buscarPorId);
+
+// Listar todos os animais
+router.get("/", animalController.listar);
+
+// Cadastrar animal
+router.post("/", animalController.cadastrar);
+
 export default router;
+
