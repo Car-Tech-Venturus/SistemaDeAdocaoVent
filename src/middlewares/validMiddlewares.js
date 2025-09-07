8831c06999c51c14ae93d3420edceff0509101c0
ArthurDombroski <tutudombroski@gmail.com>
2025-09-07 20:00:40 -0300

    feat(valid): função para validar os campos foi criada.

---
+++ src/middlewares/validMiddlewares.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+export function validarCamposObrigatorios(campos) {
+  return (req, res, next) => {
+    const faltando = [];
+
+    campos.forEach(campo => {
+      if (
+        req.body[campo] === undefined ||
+        req.body[campo] === null ||
+        req.body[campo] === ""
+      ) {
+        faltando.push(campo);
+      }
+    });
+
+    if (faltando.length > 0) {
+      return res.status(400).json({
+        erro: `Os seguintes campos são obrigatórios: ${faltando.join(", ")}`
+      });
+    }
+
+    next();
+  };
+}
+
+//ta bem básico, mas foi o que eu consegui fazer
