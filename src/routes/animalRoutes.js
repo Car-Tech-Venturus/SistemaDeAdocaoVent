a8d7960c42891d8f4b7c9f056b80e01068a66533
ArthurDombroski <tutudombroski@gmail.com>
2025-09-08 17:08:58 -0300

    refactor(animal): deixando a rota animal condizento com os midllewares

--- src/routes/animalRoutes.js
+++ src/routes/animalRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import animalController from "../controllers/animalControllers.js";
+import { autenticarToken, apenasAdmin } from "../middlewares/authMiddlewares.js"; 
+import { validarCamposObrigatorios } from "../middlewares/validMiddlewares.js"; 
 
 const router = express.Router();
 
@@ -7,23 +9,45 @@ const router = express.Router();
 //Também pq se eu não me engano, o express lê as rotas de cima pra baixo
 
 // Listar todos os animais (Admin)
-router.get("/admin/animais", animalController.listarAdmin);
+router.get("/admin/animais", autenticarToken, apenasAdmin, animalController.listarAdmin);
 
 // Atualizar animal (Admin)
-router.patch("/admin/animais/:id", animalController.atualizar);
+router.patch(
+  "/admin/animais/:id",
+  autenticarToken,
+  apenasAdmin,
+  animalController.atualizar
+);
 
 // Deletar animal (Admin)
-router.delete("/admin/animais/:id", animalController.deletar);
+router.delete(
+  "/admin/animais/:id",
+  autenticarToken,
+  apenasAdmin,
+  animalController.deletar
+);
 
 
 // Buscar animal por ID
 router.get("/:id", animalController.buscarPorId);
 
-// Listar todos os animais
+// Listar todos os animais (com filtros opcionais)
 router.get("/", animalController.listar);
 
-// Cadastrar animal
-router.post("/", animalController.cadastrar);
+// Cadastrar animal (pode ser admin também, então precisa de proteção)
+router.post(
+  "/",
+  autenticarToken,
+  apenasAdmin,
+  validarCamposObrigatorios([
+    "nome",
+    "especie",
+    "porte",
+    "castrado",
+    "vacinado",
+    "descricao"
+  ]),
+  animalController.cadastrar
+);
 
 export default router;
-
