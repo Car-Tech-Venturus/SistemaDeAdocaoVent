87b9261a5c3949207b87c6c766a79408cba9f5b4
ArthurDombroski <tutudombroski@gmail.com>
2025-09-15 17:15:32 -0300

    refactor: ocorreu uma mudança na nomenclatura das pastas

--- src/routes/tutoresRoutes.js
+++ src/routes/tutoresRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import usuarioController from "../controllers/usuarioControllers.js";
+import usuarioController from "../controllers/tutoresControllers.js";
 
 const router = express.Router();
 
