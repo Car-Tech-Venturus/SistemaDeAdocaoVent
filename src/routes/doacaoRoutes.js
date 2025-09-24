1204f56af452bfcb8105be6f0ebf986b98f35543
Mateus0825 <mateuspinheirosv2008@gmail.com>
2025-09-23 23:03:04 -0300

    feat(doacoes):rota post foi feita

--- src/routes/doacaoRoutes.js
+++ src/routes/doacaoRoutes.js
@@ -3,5 +3,8 @@ import express from "express";
 const router = express.Router();
 
 //POST: /doacoes
-
+router.post("/doacoes",
+    autenticarToken,
+    validarCamposObrigatorios(["nome", "email", "valor", "linkPix"]),
+)
 export default router;
