261eb5ae2855269b2d5739fbc4ee3aa8715d89a7
Mateus0825 <mateuspinheirosv2008@gmail.com>
2025-09-23 23:26:18 -0300

    feat(pedidoAdocao): rota criada

--- src/routes/pedidoAdocaoRoutes.js
+++ src/routes/pedidoAdocaoRoutes.js
@@ -1,5 +1,11 @@
 import express from "express"
+import { apenasAdmin } from "../middlewares/authMiddlewares";
 
 const router = express.Router();
+
+router.post("/adoacoes",
+    autenticarToken,
+    apenasAdmin,
+)
 
 export default router; 
